8604bec44de1497bb3fabb9d64facbdfc09460b1
Daniel Rybak <daniel.rybak91@gmail.com>
2021-10-10 16:21:49 +0100

    fix: scoreLabel truncating when 3 digits are displayed

--- ClearScoreTask/Controller/HomeVC.swift
+++ ClearScoreTask/Controller/HomeVC.swift
@@ -10,13 +10,16 @@ import UIKit
 class HomeVC: UIViewController {
 
   @IBOutlet weak var scoreView: UIView!
-
+  @IBOutlet weak var scoreLabel: UILabel!
+  
   override func viewDidLoad() {
     super.viewDidLoad()
     scoreView.makeCircular()
     
     scoreView.layer.borderColor = getBorderColor()
     scoreView.layer.borderWidth = 1
+    
+    scoreLabel.text = "327"
   }
   
   override func traitCollectionDidChange(_ previousTraitCollection: UITraitCollection?) {
